7ee96e50d650570a2dddaf68667ca11f652f8485
Tongle Xu <xutongle@msn.com>
2022-01-08 23:01:58 +0800

    添加常用组件

---
+++ config/autoload/crontab.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This is NOT a freeware, use is subject to license terms
+ *
+ * @copyright Copyright (c) 2018-2099 Shandong JiYuan Information Technology Co., Ltd.
+ */
+use Hyperf\Crontab\Crontab;
+
+return [
+    // 是否开启定时任务
+    'enable' => false,
+
+    'crontab' => [
+
+    ],
+];
